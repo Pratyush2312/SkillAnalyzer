332c209206f2174aa4a9c56dd8cb079da2377ac8
Pratyush <singhpratyush981@gmail.com>
2026-09-13 18:16:23 +0530

    Updated logout controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -99,7 +99,11 @@ export const getMe = async (req, res) => {
 
 export const logout = async (req, res) => { 
     try {
-        res.clearCookie('token');
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+        });
         return res.status(200).json({
             message:"User Logged out"
         })
