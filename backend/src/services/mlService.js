2927fe1ae0e82d8fe1169394b72f1aeabd86ddca
Pratyush <singhpratyush981@gmail.com>
2026-09-13 13:31:40 +0530

    Added ml service url

--- backend/src/services/mlService.js
+++ backend/src/services/mlService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const ML_SERVICE_URL = "http://127.0.0.1:5000";
+const ML_SERVICE_URL = process.env.ML_SERVICE_URL;
 
 export async function getCareerRecommendations(studentProfile) {
     try {
